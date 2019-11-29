7b74ec95ab20a0ed9a0295b621cfd1ed3da04dad
kelyu <kelyu0109@gmail.com>
2019-11-29 20:09:13 +0100

    register

--- src/main/java/proj/ssm/SsmApplication.java
+++ src/main/java/proj/ssm/SsmApplication.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@MapperScan(basePackages = {"proj.ssm.mapper"})
-//@MapperScan("proj.ssm.mapper")
 @SpringBootApplication
 public class SsmApplication {
     public static void main(String[] args){
--- src/test/java/proj/ssm/SsmApplicationTest.java
+++ src/test/java/proj/ssm/SsmApplicationTest.java
@@ -1,5 +1,57 @@
 package proj.ssm;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import proj.ssm.dao.UserMapper;
+import proj.ssm.entity.User;
+import proj.ssm.util.MailClient;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = SsmApplication.class)
 public class SsmApplicationTest {
+    /**
+     * DBTest
+     */
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testFindUser(){
+        User u= userMapper.selectById(11);
+        System.out.println(u.toString());
+    }
+
+    @Test
+    public void testAddUser(){
+        User user =new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+
+    }
+
+    /**
+     * MailTest
+     */
+    @Autowired
+    MailClient mailClient;
+    @Test
+    public void sendMailTest(){
+        mailClient.sendMail("773830355@qq.com","hello","world");
+    }
 
 }
