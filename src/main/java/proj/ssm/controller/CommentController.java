4dc2d581ebf39972bdfff56aa28c32b3d7117079
kelyu <kelyu0109@gmail.com>
2019-12-01 04:44:37 +0100

    add replay (no complete)

---
+++ src/main/java/proj/ssm/controller/CommentController.java
@@ -0,0 +1,34 @@
+package proj.ssm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import proj.ssm.entity.Comment;
+import proj.ssm.service.CommentService;
+import proj.ssm.util.HostHolder;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+
+}
--- src/main/java/proj/ssm/service/CommentService.java
+++ src/main/java/proj/ssm/service/CommentService.java
@@ -2,16 +2,24 @@ package proj.ssm.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 import proj.ssm.dao.CommentMapper;
 import proj.ssm.entity.Comment;
+import proj.ssm.util.SsmConstant;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements SsmConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
@@ -21,4 +29,23 @@ public class CommentService {
         return commentMapper.selectCountByEntity(entityType, entityId);
     }
 
+    //Propagation.REQUIRED 没有事务就新建一个，READ_COMMITTED 提交后才可读取
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+
+        // 添加评论
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // 更新帖子评论数量
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+
+        return rows;
+    }
 }
